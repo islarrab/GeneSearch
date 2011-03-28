6f4351a6498f2d306a3058a0b491ee4d762a9fed
Carlos Flores <cafp89@gmail.com>
2011-03-27 19:53:52 -0600

    panelHeader mejorado

--- src/biomatec/client/GeneView.java
+++ src/biomatec/client/GeneView.java
@@ -12,6 +12,8 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -20,6 +22,7 @@ import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+@SuppressWarnings("deprecation")
 public class GeneView extends Composite {
 	
 	protected static final String SERVER_ERROR = null;
@@ -28,14 +31,15 @@ public class GeneView extends Composite {
 	
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private VerticalPanel infoPanel = new VerticalPanel();
-	private HorizontalPanel panelHeader = new HorizontalPanel();
+	private DockPanel panelHeader = new DockPanel();
 	private ScrollPanel viewsPanel = new ScrollPanel();
+	private HorizontalPanel addViewPanel = new HorizontalPanel();
 
-	private Label genesLabel = new Label();
-	private Label databaseLabel = new Label();
+	private HTML genesLabel = new HTML();
+	private HTML databaseLabel = new HTML();
 	private Label errorLabel = new Label();
 	private ListBox viewsListBox = new ListBox();
-	private Button addViewButton = new Button("Add");
+	private Button addViewButton = new Button("+ Add View");
 	
 	private FunctionDictionary fd = new FunctionDictionary();
 	
@@ -46,20 +50,42 @@ public class GeneView extends Composite {
 		// Assemble main panel
 		mainPanel.add(panelHeader);
 		mainPanel.add(viewsPanel);
+		mainPanel.setWidth("1000px");
 
 		// Assemble the main panel header
-		panelHeader.add(infoPanel);
-		panelHeader.add(viewsListBox);
-		panelHeader.add(addViewButton);
+		panelHeader.add(infoPanel, DockPanel.WEST);
+		panelHeader.add(addViewPanel, DockPanel.EAST);
+		panelHeader.setWidth("1000px");
+		panelHeader.setStyleName("panelHeader");
+		
+		//Assemble the add view panel
+		addViewPanel.add(viewsListBox);
+		addViewPanel.add(addViewButton);
+		addViewPanel.setVerticalAlignment(VerticalPanel.ALIGN_BOTTOM);
 
 		// Assemble the info panel of the header
 		infoPanel.add(genesLabel);
 		infoPanel.add(databaseLabel);
-		genesLabel.setText("genes: "+selectedGenes.get(0).getSymbol());
+		String [] genesAvailable = dataset.getGenes().split(" ");
+		boolean found = false;
+		for (int i = 0; i < genesAvailable.length && !found; i++)
+			if (selectedGenes.get(0).getSymbol().equals(genesAvailable[i]))
+				found = true;
+		if (found)
+			genesLabel.setHTML("<b>Genes:</b> "+selectedGenes.get(0).getSymbol());
+		else
+			genesLabel.setHTML("<b>Genes:</b> <font color=\"red\">"+selectedGenes.get(0).getSymbol()+"</font>");
 		for (int i=1; i<selectedGenes.size(); i++) {
-			genesLabel.setText(genesLabel.getText()+", "+selectedGenes.get(i).getSymbol());
+			found = false;
+			for (int j = 0; j < genesAvailable.length && !found; j++)
+				if (selectedGenes.get(i).getSymbol().equals(genesAvailable[j]))
+					found = true;
+			if (found)
+				genesLabel.setHTML(genesLabel.getHTML()+" "+selectedGenes.get(i).getSymbol());
+			else
+				genesLabel.setHTML(genesLabel.getHTML()+"<font color=\"red\">"+" "+selectedGenes.get(i).getSymbol()+"</font>");
 		}
-		databaseLabel.setText("database: "+dataset.getName());
+		databaseLabel.setHTML("<b>Database:</b> "+dataset.getName());
 		
 		// Assemble the functions list box
 		fd.generateList(viewsListBox);
